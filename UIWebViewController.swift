7448f8e62efc52b940fb454d04e6be4f05bc6abb
andy_ybcao <andy_ybcao@tutorabc.com.cn>
2020-08-26 18:07:44 +0800

    add vc

---
+++ UIWebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UIWebViewController.swift
+//  CPush
+//
+//  Created by andy_ybcao on 2020/8/26.
+//
+
+import UIKit
+
+class UIWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
